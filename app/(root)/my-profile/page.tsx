6523f387a077644c41a3f00551d85f1a31f1d3a2
syedukkashah <ukkashahsyed@gmail.com>
2025-11-22 00:06:01 +0500

    db seed

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,7 +1,6 @@
 import { signOut } from "@/auth";
 import BookList from "@/components/BookList";
 import { Button } from "@/components/ui/button";
-import { sampleBooks } from "@/constants";
 import React from "react";
 
 const Page = () => {
